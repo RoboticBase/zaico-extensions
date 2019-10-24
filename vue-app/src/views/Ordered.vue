d82502446ca1e0f9de25fad611a9fbb619b93baa
Nobuyuki Matsui <nobuyuki.matsui@gmail.com>
2019-10-24 22:11:55 +0200

    refactor the Ordered view

--- vue-app/src/views/Ordered.vue
+++ vue-app/src/views/Ordered.vue
@@ -12,6 +12,21 @@
       </div>
       <p>注文を受け付けました。配送ロボット（{{data.delivery_robot.id}}）が商品を配送します。</p>
       <p>お届け先：{{ data.destination.name }}</p>
+      <p>お届けする商品</p>
+      <div class="row">
+        <div class="col-sm-6 col-md-3" v-for="item in data.updated" :key="item.id">
+          <div class="card img-thumbnail">
+            <img class="card-img-top" src="{{ item.img }}">
+            <div class="card-body px-2 py-3">
+              <h5 class="card-title">{{ item.title }}</h5>
+              <p class="card-text">カテゴリ：{{ item.category }}</p>
+              <p class="card-text">JANコード：{{ item.code }}</p>
+              <p class="card-text">倉庫：{{ item.place }}</p>
+              <p class="card-text">注文数：{{ parseInt(item.reservation) + item.unit }}</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
     <Alert/>
   </div>
@@ -32,9 +47,6 @@ export default {
     data () {
       return this.$route.params.data
     },
-    itemStr () {
-      return this.$route.params.itemStr
-    },
   },
   mounted () {
     if (!this.data) {
@@ -43,9 +55,12 @@ export default {
   },
   methods: {
     back () {
-      this.$store.commit('resetStocks')
       this.$router.push({name: 'stocks'})
     },
+  },
+  beforeRouteLeave (to, from , next) {
+    this.$store.commit('resetStocks')
+    next()
   },
 }
 </script>
