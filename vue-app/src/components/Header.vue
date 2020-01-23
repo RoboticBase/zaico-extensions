1d38bb6f9a628f50d7dc89527031930d265f236c
Nobuyuki Matsui <nobuyuki.matsui@gmail.com>
2020-01-23 18:24:39 +0900

    add unit tests for 'components/Header.vue'

--- vue-app/src/components/Header.vue
+++ vue-app/src/components/Header.vue
@@ -4,13 +4,22 @@
       <b-navbar-brand>UOA-POC2</b-navbar-brand>
       <b-navbar-nav class="ml-auto">
         <b-nav-form right>
-          <b-button variant="outline-dark" class="btn-sm" :to="{ name: 'cart' }">カート</b-button>
-          <b-button variant="outline-dark" class="btn-sm" :to="{ name: 'histories' }">注文履歴</button>
+          <b-button variant="outline-dark" class="btn-sm cart" :to="{ name: 'cart' }">カート</b-button>
+          <b-button variant="outline-dark" class="btn-sm histories" :to="{ name: 'histories' }">注文履歴</b-button>
         </b-nav-form>
       </b-navbar-nav>
     </b-navbar>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'hdr',
+  data: function () {
+    return {}
+  },
+}
+</script>
 
 <style scoped>
 div.header {
