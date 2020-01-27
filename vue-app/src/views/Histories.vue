f6273fea4b81db76f017568bbe700ea20694dcc8
Nobuyuki Matsui <nobuyuki.matsui@gmail.com>
2020-01-27 19:55:11 +0900

    add unit tests for 'views/Histories.vue'

--- vue-app/src/views/Histories.vue
+++ vue-app/src/views/Histories.vue
@@ -5,17 +5,17 @@
     <div v-for="(order, idx) in ordered" :key="idx">
       <b-card no-body class="mx-auto">
         <b-card-header header-tag="header" class="p-1" role="tab">
-          <b-button block href="#" v-b-toggle="'accordion-'+idx" variant="default">{{ order.orderDate }}のご注文</b-button>
+          <b-button block href="#" v-b-toggle="'accordion-'+idx" variant="default" class="orderDate">{{ order.orderDate }}のご注文</b-button>
         </b-card-header>
         <b-collapse v-bind:id="'accordion-'+idx" accordion="my-accordion" role="tabpanel">
           <b-card-body>
-            <b-card-text>{{ order.destination.name }}へお届け</b-card-text>
+            <b-card-text class="destination">{{ order.destination.name }}へお届け</b-card-text>
             <div v-for="item in order.updated" :key="item.id">
-              <b-card-text><b>{{ item.title}}</b><br/>を{{ item.place }}から{{ parseInt(item.reservation) + item.unit }}</b-card-text>
+              <b-card-text class="item"><b>{{ item.title}}</b><br/>を{{ item.place }}から{{ parseInt(item.reservation) + item.unit }}</b-card-text>
             </div>
             <div>
               <hr/>
-              <b-card-text>受取コード</b-card-text>
+              <b-card-text class="qrcode">受取コード</b-card-text>
               <vue-qrcode :value="targetText(order)" :options="option" tag="img"></vue-qrcode>
             </div>
           </b-card-body>
@@ -64,7 +64,7 @@ export default {
       let items = order.updated.reduce((acc, current) => {
         return acc + '[title:' + current.title + ', num:' + current.reservation + '] '
       }, '')
-      return order.orderDate + ' robot:' + order.delivery_robot.id + ' dest:' + order.destination.name + ' items: ' + items
+      return order.orderDate + ' robot:' + order.delivery_robot.id + ' dest:' + order.destination.name + ' items:' + items
     },
   },
 }
